a2af1e98899002f17fa5c1aa25d4b8d53e819e4a
apeng0018 <80731248+apeng0018@users.noreply.github.com>
2021-10-12 05:15:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,10 +10,10 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+ Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-Add a feed source
+ Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
