e340b4bd97691c40745bb47b574f8cdbeadeff2c
apeng0018 <80731248+apeng0018@users.noreply.github.com>
2021-10-12 04:43:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,9 +13,9 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
- Add a feed source
+Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages
-sed -i '$a src-git small https://github.com/kenzok8/small
-#sed -i '$a src-git wiringPi https://github.com/nmaas87/rpi-openwrt-packages.git
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git wiringPi https://github.com/nmaas87/rpi-openwrt-packages.git' feeds.conf.default
