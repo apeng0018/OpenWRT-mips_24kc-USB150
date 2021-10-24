bef06f676afbe1a3f23206573db4ed6624303e24
apeng0018 <80731248+apeng0018@users.noreply.github.com>
2021-10-24 20:54:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
  Uncomment a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld'>>feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
-
+ 
  Add a feed sourcesed -i '$a 
 #src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
